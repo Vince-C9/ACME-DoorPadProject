a3fda3a4a36ac7e1fd2dd918fb88e974751936dd
vince <vincecole6@hotmail.co.uk>
2024-01-15 02:56:58 +0000

    Latest version of "runner" document.

---
+++ api/config/acme.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'path'=>'acme_access',
+    'door_key_errors' => [
+        'required'=>'You must provide an access key.',
+        'number'=>'Your access key must be a number.',
+        'size'=>'Your access key must be six digits.',
+        'exists' => 'Invalid access Key.'
+    ],
+];
+
+
